9ce926e0626efe7976955a0eed7d1899016826cf
tbrittain2 <tbrittain2@ucmerced.edu>
2021-10-05 11:49:06 -0700

    rewrote a/b to be strings by default

--- Lab_4/script.js
+++ Lab_4/script.js
@@ -1,8 +1,9 @@
 // a (operation) b
 let operation = '+'
-let a = 0
-let b = 0
+let a = '0'
+let b = '0'
 let display_val = 0
+let last = 'ENTER'
 
 createListeners()
 
@@ -17,52 +18,60 @@ function createListeners() {
 
     // Gather all buttons
     const buttons = document.querySelectorAll('.grid-item')
+    let last = false
     
     // assigning listeners to each
     buttons.forEach(ele => {
         ele.addEventListener('click', () => {
-            // console.log('pre-op')
-            // console.log(`sum:${sum}\ndispaly:${display_val}\nop:${operation}`)
+
 
             //manipulating everything based on inner text value
             const val = ele.innerText
 
             // change operation
             if (val === 'x' || val === '/' || val === '+' || val === '-'){
-                sum = display_val
-                display_val = 0
+                if (last !== 'ENTER')
+                    a = String(execute(a,b,operation))
+                b = 0
                 operation = val
             }
             else if(val === '(-)'){
-                display_val *= -1
+                b *= -1
             }
         
             else if(val === 'CLR'){
-                sum = 0
-                display_val = 0
+                a = 0
+                b = 0
                 operation = '+'
             }
             else if(val === 'ENTER'){
-                display_val = execute(sum,display_val,operation)
+                a = String(execute(a,b,operation))
             }
-            else if(val === '.'){
-                console.log('HANDLE ME')
-            }
+            // else if(val === '.'){
+            //     console.log('HANDLE ME')
+            // }
             else{
                 appendVal(val)
             }
             //Enter
             //Else (numeric)
-            display.innerText = display_val
+            last = val
+            display.innerText = ((val === 'ENTER' || val === 'x' || val === '/' || val === '+' || val === '-') ? a:b)
             console.log('post-op')
-            console.log(`sum:${sum}\ndispaly:${display_val}\nop:${operation}`)
+            console.log(`a:${a}\nb:${b}\nop:${operation}`)
 
         })
     });
 }
 
 function appendVal(val){
-    display_val = Number(display.innerText) * 10 + Number(val)
+    // b = b * 10 + Number(val)
+    // console.log('FUCK')
+    b = String(b)
+    if (b.charAt(0) === '0')
+        b = b.substr(1,b.length)
+    b += val 
+    // console.log('ME')
 }
 
 
@@ -86,7 +95,7 @@ function execute(a, b, op){
             return subtract(a,b)
         case '/': 
             return divide(a,b)
-        case '*':
+        case 'x':
             return multiply(a,b)
         default:
             return 'EXECUTE ERROR'
