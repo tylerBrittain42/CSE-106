f15b06634d8099430b5340655d1eee694c23987c
tbrittain2 <tbrittain2@ucmerced.edu>
2021-10-12 14:30:16 -0700

    implemented view-all page

--- Lab_5/script.js
+++ Lab_5/script.js
@@ -116,31 +116,3 @@ function deleteStudent(stu_name) {
 
 }
 
-function jsonToList() {
-  return fetch("https://amhep.pythonanywhere.com/grades")
-    .then((res) => {
-      const results = res.json();
-      // console.log(typeof results);
-      // console.log(results);
-      return results;
-    })
-    .then((results) => {
-      const llist = [];
-      for (let i of Object.keys(results)) {
-        llist.push({ i: results[i] });
-      }
-      // console.log(llist)
-      return llist;
-    });
-}
-
-async function a() {
-  res = await fetch("https://amhep.pythonanywhere.com/grades");
-  data = await res.json();
-  const llist = [];
-
-  for (let i of Object.keys(data)) {
-    llist.push({ i: data[i] });
-  }
-}
-
