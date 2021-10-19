6fae3697d0583dd897846fe6ab6e9b0a21e528a2
tbrittain2 <tbrittain2@ucmerced.edu>
2021-10-19 11:05:54 -0700

    Requests(no error handling, no persistent data) implemented

--- lab_6/server/app.py
+++ lab_6/server/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+
+# NOTE: using a temporary grades object
+# have not used error handling yet
+grades = {'Tyler':100,'Steve':40}
 
 
 @app.route('/')
@@ -13,26 +18,32 @@ def hello_world():
 
 # GET and POST /grades
 @app.route('/grades', methods=["GET","POST"])
-def grade_stuff():
+def grade_stuff(): 
     if request.method == 'GET':
-        print(request)
-        return "GOT grades"
+        return grades
     elif request.method == 'POST':
+        content = request.get_json(force=True)
+        grades[content['name']] = content['grade']
+        print(grades)
         return('Grade post recieved')
 
 
 # GET /grades/<Student name>
 @app.route('/grades/<name>')
 def student_grade(name):
-    return f"GOT {name}'s grades"
+    return {name : grades[name]}
 
 
 # PUT /grades/<Student Name>
 @app.route('/grades/<name>', methods=["PUT"])
 def put_student(name):
+    content = request.get_json(force=True)
+    grades[name] = content['grade']
+    print(grades)
     return(f"PUT for {name} recieved")
 
-# PUT /grades/<Student Name>
+# DELETE /grades/<Student Name>
 @app.route('/grades/<name>', methods=["DELETE"])
 def delete_student(name):
+    del grades[name]
     return(f"DELETE for {name} recieved")
