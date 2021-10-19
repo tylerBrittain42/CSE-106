9fc5f8f720351421b0d9ecc63a8029faf41c0fb3
tbrittain2 <tbrittain2@ucmerced.edu>
2021-10-19 10:13:48 -0700

    all routes created

---
+++ lab_6/server/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "<h1> Index </h1>"
+
+
+
+# GET and POST /grades
+@app.route('/grades', methods=["GET","POST"])
+def grade_stuff():
+    if request.method == 'GET':
+        return "GOT grades"
+    elif request.method == 'POST':
+        return('Grade post recieved')
+
+
+# GET /grades/<Student name>
+@app.route('/grades/<name>')
+def student_grade(name):
+    return f"GOT {name}'s grades"
+
+
+# PUT /grades/<Student Name>
+@app.route('/grades/<name>', methods=["PUT"])
+def put_student(name):
+    return(f"PUT for {name} recieved")
+
+# PUT /grades/<Student Name>
+@app.route('/grades/<name>', methods=["DELETE"])
+def delete_student(name):
+    return(f"DELETE for {name} recieved")
